c1c854fa443f94bb4bbe7211a230c3954551adfc
urielkwok <urielkwok@gmail.com>
2026-03-04 03:52:50 -0500

    Update data_loader.py
    Added an option to easily modify the timeframe of the data (What year to start from, number of years to iterate through)

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,24 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime
+
+START_YEARS_AGO = 2
+END_YEARS_AGO = 0
+
+
+def get_dates():
+    """
+    Requires: Nothing
+    Modifies: Nothing
+    Effects: Returns correctly formatted start and end dates.
+    """
+    today = datetime.today()
+    start_date = (today.replace(year=today.year - START_YEARS_AGO))
+    modified_start_date = start_date.strftime("%Y-%m-%d")
+    end_date = (today.replace(year=today.year - END_YEARS_AGO))
+    modified_end_date = end_date.strftime("%Y-%m-%d")
+
+    return modified_start_date, modified_end_date
 
 
 def get_data(stock_1: str, stock_2: str, start_date: str, end_date: str) -> pd.DataFrame:
