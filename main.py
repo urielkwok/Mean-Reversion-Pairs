5d57b5dddfb639afa182a6abf7c29637bd1769c8
urielkwok <urielkwok@gmail.com>
2026-03-05 17:20:23 -0500

    Update main.py
    Uses new visualization tools for z-values

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import src.data_loader as dl
 import src.analysis as an
+import src.visualizer as vz
 
 START_DATE, END_DATE = dl.get_dates()
 STOCK_1 = "KO"
@@ -10,6 +11,7 @@ spread = stock_df[STOCK_1] - stock_df[STOCK_2]
 stationary = an.adf_test(spread)
 if stationary is True:
     print("Spread is stationary")
-    print(an.z_score(spread))
+    z_score = an.z_score(spread)
+    vz.plot_z_values(z_score)
 else:
     print("Spread is not stationary.")
