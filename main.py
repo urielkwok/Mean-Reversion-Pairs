5c64a300a28a33523ecb21c37c637407d7092c79
urielkwok <urielkwok@gmail.com>
2026-03-07 17:17:17 -0500

    Added OLS to find alpha and beta for the spread

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ STOCK_1 = "KO"
 STOCK_2 = "PEP"
 
 stock_df = dl.get_data(STOCK_1, STOCK_2, START_DATE, END_DATE)
-spread = stock_df[STOCK_1] - stock_df[STOCK_2]
+alpha, beta = an.OLS_regression(stock_df[STOCK_1], stock_df[STOCK_2])
+spread = stock_df[STOCK_2] - (alpha + beta * stock_df[STOCK_1])
 stationary = an.adf_test(spread)
 if stationary is True:
     print("Spread is stationary")
--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,5 @@
 import numpy as np
-from statsmodels.tsa.stattools import adfuller as adf
+import statsmodels.tsa.stattools as sm
 
 
 def adf_test(series: np.NDArray) -> bool:
@@ -8,12 +8,26 @@ def adf_test(series: np.NDArray) -> bool:
     Modifies: Nothing
     Returns: True if series is stationary, False otherwise
     """
-    result = adf(series)
+    result = sm.adfuller(series)
     print(f"Raw ADF: {result[0]:.4f}, p-value: {result[1]:.4f}")
     if result[1] < 0.05:
         return True
     else:
         return False
+ 
+
+def OLS_regression(ind_stock: np.NDArray, dep_stock: np.NDArray) -> tuple[float, float]:
+    """
+    Requires: Nothing
+    Modifies: Nothing
+    Returns: Alpha and Beta values for the two variables
+    """
+    x = sm.add_constant(ind_stock)
+    y = dep_stock
+    regression = sm.OLS(x, y).fit()
+    alpha = regression.params[0]
+    beta = regression.params[1]
+    return alpha, beta
 
 
 def z_score(series: np.ndarray) -> np.NDArray[np.float64]:
